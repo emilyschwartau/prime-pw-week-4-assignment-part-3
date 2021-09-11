64b0dc90bfc811c93c67d5d02163cae7d8540fdd
Emily Schwartau <emilyschwartau+1@gmail.com>
2021-09-11 16:31:51 -0600

    completed week 4 assignment part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,35 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+//create global variable basket array
+let basket = [];
+console.log(basket);
+
+//create function addItem
+function addItem( item ) {
+  basket.push( item );
+  return true;
+}//end addItem
+console.log('Test - should show "true"', addItem( "pears" ));
+console.log(basket);
+
+addItem("apples");
+addItem("bananas");
+console.log(basket);
+
+//create function listItems
+function listItems( basket ) {
+  for (var i = 0; i < basket.length; i++) {
+    console.log(basket[i]);
+  }
+  return basket[i];
+}
+console.log('Test', listItems( basket ));
+
+//create function empty
+function empty( basket ) {
+  basket = [];
+  return basket;
+}//end empty
+console.log('Test', empty( basket ));
